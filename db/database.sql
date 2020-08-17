2e4006e2041016ff16a2e9ba25bcf22a8aa7f2f4
Shakhawat <sh.sumon99@gmail.com>
2020-08-17 13:14:43 +0600

    database script added

---
+++ db/database.sql
@@ -0,0 +1,42 @@
+/*
+SQLyog Enterprise - MySQL GUI v8.18 
+MySQL - 5.7.24 : Database - node_api
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `employees` */
+
+DROP TABLE IF EXISTS `employees`;
+
+CREATE TABLE `employees` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `organization` varchar(255) NOT NULL,
+  `designation` varchar(100) NOT NULL,
+  `salary` decimal(11,2) unsigned DEFAULT '0.00',
+  `status` tinyint(3) unsigned DEFAULT '0',
+  `is_deleted` tinyint(3) unsigned DEFAULT '0',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `employees` */
+
+insert  into `employees`(`id`,`first_name`,`last_name`,`email`,`phone`,`organization`,`designation`,`salary`,`status`,`is_deleted`,`created_at`,`updated_at`) values (1,'John','Doe','johndoe@gmail.com','1234567890','ABC Tech','Full Stack Developer','1150.00',1,0,'2020-08-12 03:30:30','2020-08-17 13:10:36'),(2,'Shakhawat','Mollah','janedoe@gmail.com','+8801816139037','qCodex Inc.','Software Developer','1200.00',1,0,'2020-08-12 03:35:30','2020-08-17 13:10:52');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
